e9ed200b0853c7260014e948190ccda9ab4ae380
nfrank5 <nfrankenfeld@gmail.com>
2022-07-10 19:19:54 -0300

    Add Mastermind Computer as Codebreaker

--- mastermind.rb
+++ mastermind.rb
@@ -38,16 +38,23 @@ end
 class Game
 
   def initialize
-
     @allow_duplicates = false
     @hidden_code_pattern = []
     @feedback = []
     @guess_pattern = ''
+    @combinations = [1, 2, 3, 4, 5, 6].repeated_permutation(4).to_a
   end
 
   def welcome
+    choose_maker_breaker = ''
     puts 'Welcome to Mastermind Game!'
     puts 'The game where you need to discover a secret pattern of four colours'
+    puts 'Do you want to be the Codemaker or the Codebreaker?'
+    until choose_maker_breaker.downcase.match(/^[1-2]$/)
+      puts 'Write 1 to be the Codemaker or 2 to be the Codebreaker'
+      choose_maker_breaker = gets.chomp
+    end
+    choose_maker_breaker.to_i
   end
 
   def player_guess_pattern
@@ -67,10 +74,21 @@ class Game
     code
   end
 
+  def player_create_hidden_code
+    code_string = ''
+    puts 'Choose four of the following colors to creat the hidden pattern.'
+    CODE_PEGS.each_index { |index| puts "#{index.to_i + 1}. #{CODE_PEGS[index].capitalize}" }
+    until code_string.match(/^[1-6]{4}$/)
+      puts 'Write only four digits without spaces'
+      code_string = gets.chomp
+    end
+    code_string.split('').map(&:to_i)
+  end
+
   def feedback(guess, hidden_code)
     black_key_pegs = []
     white_key_pegs = []
-    multiple_white_key_pegs = []
+    multiple_white_key_pegs = [] # Used to show the correct amount of white pegs when guess contains repeated colors
     final_key_pegs = []
     guess.each_index do |i|
       if guess[i] == hidden_code[i]
@@ -98,31 +116,83 @@ class Game
   end
 
   def winner?(feedback)
-    feedback.count('black') == 4
+    feedback.count(0) == 4
+  end
+                           
+  def computer_guess_pattern(feedback, last_guess, hidden_code_pattern) 
+    new_guess = []
+    @combinations = @combinations.select { |element| feedback(element, last_guess) == feedback(last_guess, hidden_code_pattern) }
+    @combinations[0]
   end
 
   def play_game
+
+    type_of_game = welcome
+    p type_of_game
+    if  type_of_game == 2
+      play_game_codebraker
+    else
+      play_game_codemaker
+    end
+
+  end
+
+  def play_game_codebraker
     game_board = Board.new
-    fb = []
-    welcome
     @hidden_code_pattern = generate_hidden_code_pattern
     12.downto(1) do |i|
       clear_screen
       # p @hidden_code_pattern
       game_board.print_board
-      puts "You have #{i} " + (i==1?"guess":"guesses") + " left"
+      puts "You have #{i} #{i == 1 ? 'guess' : 'guesses'} left"
       @guess_pattern = player_guess_pattern 
-      fb = feedback(@guess_pattern, @hidden_code_pattern)
-      game_board.add_pattern(@guess_pattern, fb)
-      if winner?(fb)
-        puts "You Won"
+      @feedback = feedback(@guess_pattern, @hidden_code_pattern)
+      game_board.add_pattern(@guess_pattern, @feedback)
+      if winner?(@feedback)
+        puts 'You Won, press Enter.'
+        gets
         break
       end
     end
     clear_screen
-    puts 'Game Over'
+    puts 'Final board'
     game_board.print_board
+    puts 'Game Over'
   end
+
+  def play_game_codemaker
+    game_board = Board.new
+
+    @hidden_code_pattern = player_create_hidden_code
+    last_guess = []
+    12.downto(1) do |i|
+      clear_screen
+      # p @hidden_code_pattern
+      game_board.print_board
+      puts "Computer has #{i} #{i == 1 ? 'guess' : 'guesses'} left"
+      if last_guess ==[]
+        @guess_pattern = [1, 1, 2, 2]
+      else
+        @guess_pattern = computer_guess_pattern(@feedback, last_guess, @hidden_code_pattern)
+      end
+      @feedback = feedback(@guess_pattern, @hidden_code_pattern)
+      game_board.add_pattern(@guess_pattern, @feedback)
+      last_guess = @guess_pattern
+      if winner?(@feedback)
+        puts 'Computer Won, press Enter.'
+        gets
+        break
+      end
+    end
+    clear_screen
+    puts 'Final board'
+    game_board.print_board
+    
+    puts 'Game Over'
+  end
+
+
+
 end
 
 juego = Game.new
